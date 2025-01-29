6909176b7336d454d5d7911a75d95231d9825d36
vishvam <110680520+vishvam7738@users.noreply.github.com>
2025-01-29 17:23:16 +0530

    Created 29jan.cpp

---
+++ jan'25/29jan.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(unordered_map<int,vector<int>>& adj,int u,int v,vector<bool>& vis){
+        vis[u]=true;
+        if(u==v) return true;
+        for(auto it:adj[u]){
+            if(vis[it]) continue;
+            if(dfs(adj,it,v,vis)) return true;
+        }
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        unordered_map<int, vector<int>> adj;
+        for(int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            vector<bool> vis(n,false);
+            if(adj.find(u)!=adj.end()&&adj.find(v)!=adj.end()&&dfs(adj,u,v,vis)){
+                return edges[i];
+            }
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return {};
+    }
+};
