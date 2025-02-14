46b95b26c680ab9e9f2e70eded0a49ddca354b14
vishvam <110680520+vishvam7738@users.noreply.github.com>
2025-02-14 09:57:50 +0530

    Created 14feb.cpp

---
+++ feb'25/14feb.cpp
@@ -0,0 +1,27 @@
+class ProductOfNumbers {
+public:
+    vector<int> v;
+    ProductOfNumbers() {
+        
+    }
+    
+    void add(int num) {
+        v.push_back(num);
+    }
+    
+    int getProduct(int k) {
+        int ans=1;
+        int j=v.size()-1;
+        while(k--){
+            ans*=v[j--];
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
