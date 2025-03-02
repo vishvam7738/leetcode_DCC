445336ffd23a8129ad68cb613a2b0b26660e8766
vishvam <110680520+vishvam7738@users.noreply.github.com>
2025-03-02 11:56:17 +0530

    Created 2mar.cpp

---
+++ mar'25/2mar.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        int i=0,j=0;
+        vector<vector<int>> ans;
+        while(i<n1&&j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.push_back({nums1[i][0],nums1[i][1]});
+                i++;
+            }
+            else{
+                ans.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+            }
+        }
+        while(i<n1){
+            ans.push_back({nums1[i][0],nums1[i][1]});
+            i++;
+        }
+        while(j<n2){
+            ans.push_back({nums2[j][0],nums2[j][1]});
+            j++;
+        }
+        return ans;
+    }
+};
