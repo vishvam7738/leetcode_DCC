67a9d2753d7ef991baf4739fa3be6a6c8b9aa284
vishvam <110680520+vishvam7738@users.noreply.github.com>
2025-03-11 06:00:37 +0530

    Created 11mar.cpp

---
+++ mar'25/11mar.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        int lastSeen[3]={-1,-1,-1};
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            lastSeen[s[i]-'a']=i;
+            // if(lastSeen[0]!=-1&&lastSeen[1]!=-1&&lastSeen[2]!=-1){
+                cnt=cnt+(1+min(lastSeen[0],min(lastSeen[1],lastSeen[2])));
+            // }
+        }
+        return cnt;
+    }
+};
